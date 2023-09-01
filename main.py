ddbcabea20e47ddc5ec51ed7c4d08fed71524bfd
Donovan Nguyen <shiiirro.donovan@gmail.com>
2023-08-31 23:46:17 -0700

    add optional ptr movement cooldown

--- main.py
+++ main.py
@@ -7,10 +7,14 @@ from collections import deque
 
 pydirectinput.FAILSAFE = False
 
+#######################################################
 # constants/settings, modify as needed
-ptr_sens = 15000
-moving_average_window_size = 5
-shoot_movement_cd = 3 # TODO
+PTR_SENS = 15000
+MOVING_AVG_WINDOW_LEN = 5
+COOLDOWN_MAX = 0    # set to 0 to disable
+                    # disables mouse movement for a short time after transitioning to a SHOOT pose
+                    # makes aiming at static targets easier but makes tracking more difficult
+#######################################################
 
 def main():
     model = HandModel(debug = True)
@@ -18,10 +22,11 @@ def main():
     if not cap.isOpened(): exit()
 
     # moving average to smooth model coordinates stream
-    window = deque(maxlen = moving_average_window_size)
+    window = deque(maxlen = MOVING_AVG_WINDOW_LEN)
 
     prev_ptr_coord = []
     shooting = False
+    cooldown = 0
 
     while True:
         online, frame = cap.read()
@@ -32,24 +37,26 @@ def main():
         frame = model.mark_on(frame)
 
         if model.raw_results.hand_landmarks and model.pose != "HOVER":
-            window.append(model.coordinates[8])
-            ptr_coord = np.average(window, axis = 0)
-
-            # calculation of pixel movement from previous coordinates
-            rel_move = (int((prev_ptr_coord[2] - ptr_coord[2]) * ptr_sens),
-                        int((prev_ptr_coord[1] - ptr_coord[1]) * ptr_sens)) if len(
-                prev_ptr_coord) > 0 else (
-                0, 0)
-
             # pose processing
             if model.pose == "SHOOT" and not shooting:
                 pydirectinput.mouseDown(_pause = False)
                 shooting = True
+                cooldown = COOLDOWN_MAX
             elif model.pose != "SHOOT" and shooting:
                 pydirectinput.mouseUp(_pause = False)
                 shooting = False
             if model.pose == "RELOAD":
                 pydirectinput.press('r')
+
+            if cooldown <= 0:
+                window.append(model.coordinates[8])
+            ptr_coord = np.average(window, axis = 0)
+
+            # calculation of pixel movement from previous coordinates
+            rel_move = (int((prev_ptr_coord[2] - ptr_coord[2]) * PTR_SENS),
+                        int((prev_ptr_coord[1] - ptr_coord[1]) * PTR_SENS)) if len(
+                prev_ptr_coord) > 0 else (
+                0, 0)
 
             # relative mouse movement
             pydirectinput.moveRel(*rel_move, relative = True, _pause = False,
@@ -57,13 +64,15 @@ def main():
 
             prev_ptr_coord = ptr_coord
         else:
+            window.clear()
+            prev_ptr_coord = []
             if shooting:
                 pydirectinput.mouseUp(_pause = False)
                 shooting = False
-            window.clear()
-            prev_ptr_coord = []
+            cooldown = 0
 
         cv.imshow('live', frame)
+        cooldown -= 1
 
         # exit key
         if cv.waitKey(1) == ord('q'):
